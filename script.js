dd756fc25765546680ea1fb893cd6a0acf047b34
Ariane2023 <anne.de.abreu@gmail.com>
2025-01-22 08:43:25 -0300

    Adicionando a parte de despesa na lista.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Seleciona elementos do formulário.
-const amount = document.getElementById("amount");
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
 // Captura o evemto de input para formatar o valor.
 amount.oninput = () => {
@@ -22,4 +25,21 @@ function formatCurrencyBRL(value){
 
   // retorna o valor formatado.
   return value
+}
+
+// Captura o evento de submit do formulário para obter os valores.
+form.onsubmit = (Event) => {
+  // Previne o comportamento padrão de recarregar a página.
+  Event.preventDefault()
+
+  // Cria um objeto com os detalhes na nova despesa.
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    create_at: new Date(),
+  }
+
 }
